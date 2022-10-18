e81e0127f5c7fc341ae5998380926c23c824225e
Jaryt <jarytbustard@gmail.com>
2022-10-18 13:02:19 -0400

    chore: exported scripts

---
+++ src/scripts/push-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+IFS="," read -ra DOCKER_TAGS <<< "$PARAM_TAG"
+
+for tag in "${DOCKER_TAGS[@]}"; do
+    docker push "$PARAM_REGISTRY_URL/${!EVAL_PROJECT_ID}/$PARAM_IMAGE:${tag}"
+done
+
+if [ -n "$PARAM_DIGEST_PATH" ]; then
+    mkdir -p "$(dirname $PARAM_DIGEST_PATH)"
+    docker image inspect --format="{{index .RepoDigests 0}}" "$PARAM_REGISTRY_URL/${!EVAL_PROJECT_ID}/$PARAM_IMAGE:${DOCKER_TAGS[0]}" > "$PARAM_DIGEST_PATH"
+fi
