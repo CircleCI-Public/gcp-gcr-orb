0c0d06b2f42c7e090f2a7664994e5195c3d1ff73
Jaryt <jarytbustard@gmail.com>
2022-10-18 15:52:32 -0400

    fix: eval sample inspection

--- src/scripts/push-image.sh
+++ src/scripts/push-image.sh
@@ -3,7 +3,6 @@
 IFS="," read -ra DOCKER_TAGS <<< "$EVAL_TAG"
 PROJECT_ID="${!INDIRECT_PROJECT_ID}"
 
-set -x
 for tag_to_eval in "${DOCKER_TAGS[@]}"; do
     TAG=$(eval echo "$tag_to_eval")
     docker push "$PARAM_REGISTRY_URL/$PROJECT_ID/$PARAM_IMAGE:$TAG"
@@ -11,5 +10,6 @@ done
 
 if [ -n "$PARAM_DIGEST_PATH" ]; then
     mkdir -p "$(dirname "$PARAM_DIGEST_PATH")"
-    docker image inspect --format="{{index .RepoDigests 0}}" "$PARAM_REGISTRY_URL/$PROJECT_ID/$PARAM_IMAGE:${DOCKER_TAGS[0]}" > "$PARAM_DIGEST_PATH"
+    SAMPLE_FIRST=$(eval echo "${DOCKER_TAGS[0]}")
+    docker image inspect --format="{{index .RepoDigests 0}}" "$PARAM_REGISTRY_URL/$PROJECT_ID/$PARAM_IMAGE:$SAMPLE_FIRST" > "$PARAM_DIGEST_PATH"
 fi
