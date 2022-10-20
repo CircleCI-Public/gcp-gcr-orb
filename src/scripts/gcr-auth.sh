7d83da4e8efc8d7cb461d6f560f9a1e98e1e5d5b
Jaryt <jarytbustard@gmail.com>
2022-10-20 16:12:31 -0400

    feat!: decouple from gcp cli

--- src/scripts/gcr-auth.sh
+++ src/scripts/gcr-auth.sh
@@ -3,6 +3,39 @@
 # Set sudo to work whether logged in as root user or non-root user
 if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 
+
+# Check if the AWS CLI is installed
+if ! command -v gcloud &>/dev/null; then
+  echo "GCP CLI could not be found. Please install it before initiating the GCP CLI.
+  
+Use the circleci/gcp-cli orb to install the GCP CLI.
+https://circleci.com/developer/orbs/orb/circleci/gcp-gcr#usage-examples
+
+Job example:
+
+jobs:
+    build_app:
+        executor: gcp-gcr/default
+        steps:
+            - checkout
+
+            - gcp-cli/setup
+
+            - gcp-gcr/gcr-auth
+
+
+Workflow example:
+
+workflows:
+    test_and_deploy:
+        jobs:
+        - gcp-gcr/add-image-tag:
+            pre-steps:
+                - gcp-cli/setup
+"
+  exit 1
+fi
+
 # configure Docker to use gcloud as a credential helper
 mkdir -p "$HOME/.docker"
 
