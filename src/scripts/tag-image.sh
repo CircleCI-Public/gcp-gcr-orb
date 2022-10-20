842f9514a99ec277731781b7ddc231e85b8cdb62
Jaryt <jarytbustard@gmail.com>
2022-10-20 12:45:18 -0400

    chore: added new line to tag-image

--- src/scripts/tag-image.sh
+++ src/scripts/tag-image.sh
@@ -7,3 +7,4 @@ IMAGE_ROOT="$ORB_VAL_REGISTRY_URL/${!ORB_ENV_PROJECT_ID}/$ORB_VAL_IMAGE"
 gcloud container images add-tag --quiet \
     "$IMAGE_ROOT:$SOURCE_TAG" \
     "$IMAGE_ROOT:$TARGET_TAG"
+    
