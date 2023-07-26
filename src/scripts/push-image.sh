347f94555878c6b0e4afb3ad499d1ead5bfd37ca
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-07-26 14:35:18 -0300

    refactor: include `set -x`

--- src/scripts/push-image.sh
+++ src/scripts/push-image.sh
@@ -9,11 +9,15 @@ PROJECT_ID="${!ORB_ENV_PROJECT_ID}"
 
 for tag_to_eval in "${DOCKER_TAGS[@]}"; do
     TAG=$(eval echo "$tag_to_eval")
+    set -x
     docker push "$ORB_VAL_REGISTRY_URL/$PROJECT_ID/$ORB_VAL_IMAGE:$TAG"
+    set +x
 done
 
 if [ -n "$ORB_VAL_DIGEST_PATH" ]; then
     mkdir -p "$(dirname "$ORB_VAL_DIGEST_PATH")"
     SAMPLE_FIRST=$(eval echo "${DOCKER_TAGS[0]}")
+    set -x
     docker image inspect --format="{{index .RepoDigests 0}}" "$ORB_VAL_REGISTRY_URL/$PROJECT_ID/$ORB_VAL_IMAGE:$SAMPLE_FIRST" > "$ORB_VAL_DIGEST_PATH"
+    set +x
 fi
