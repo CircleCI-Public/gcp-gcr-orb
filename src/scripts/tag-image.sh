d789871c571a17fef05a1f7a268942b1694595fe
Peter Burns <peterb@oanda.com>
2023-07-28 13:30:13 -0400

    fix: tag-image parameter expansion fix using `circleci env subst`

--- src/scripts/tag-image.sh
+++ src/scripts/tag-image.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-SOURCE_TAG=$(eval echo "$ORB_EVAL_SOURCE_TAG")
-TARGET_TAG=$(eval echo "$ORB_EVAL_TARGET_TAG")
+ORB_VAL_REGISTRY_URL="$(circleci env subst "$ORB_VAL_REGISTRY_URL")"
+ORB_VAL_IMAGE="$(circleci env subst "$ORB_VAL_IMAGE")"
+
+SOURCE_TAG="$(circleci env subst "$ORB_EVAL_SOURCE_TAG")"
+TARGET_TAG="$(circleci env subst "$ORB_EVAL_TARGET_TAG")"
 
 IMAGE_ROOT="$ORB_VAL_REGISTRY_URL/${!ORB_ENV_PROJECT_ID}/$ORB_VAL_IMAGE"
 gcloud container images add-tag --quiet \
     "$IMAGE_ROOT:$SOURCE_TAG" \
     "$IMAGE_ROOT:$TARGET_TAG"
-    
