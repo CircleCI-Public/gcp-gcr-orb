d992d026e4b8d7288e1830e01ee33ed5a09e351c
Peter Burns <peterb@oanda.com>
2023-08-08 09:44:28 -0400

    fix: revert SOURCE_TAG and TARGET_TAG to use `eval echo`

--- src/scripts/tag-image.sh
+++ src/scripts/tag-image.sh
@@ -3,8 +3,8 @@
 ORB_VAL_REGISTRY_URL="$(circleci env subst "$ORB_VAL_REGISTRY_URL")"
 ORB_VAL_IMAGE="$(circleci env subst "$ORB_VAL_IMAGE")"
 
-SOURCE_TAG="$(circleci env subst "$ORB_EVAL_SOURCE_TAG")"
-TARGET_TAG="$(circleci env subst "$ORB_EVAL_TARGET_TAG")"
+SOURCE_TAG=$(eval echo "$ORB_EVAL_SOURCE_TAG")
+TARGET_TAG=$(eval echo "$ORB_EVAL_TARGET_TAG")
 
 IMAGE_ROOT="$ORB_VAL_REGISTRY_URL/${!ORB_ENV_PROJECT_ID}/$ORB_VAL_IMAGE"
 gcloud container images add-tag --quiet \
