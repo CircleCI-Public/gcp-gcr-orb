7a6ce2aafa367b1df295e3ec222cfaa406b49728
David Alberto Montaño Fetecua <david.montano@circleci.com>
2024-10-03 21:38:58 -0500

    fix: checking how the tag command is working

--- src/scripts/tag-image.sh
+++ src/scripts/tag-image.sh
@@ -7,6 +7,10 @@ SOURCE_TAG=$(eval echo "$ORB_EVAL_SOURCE_TAG")
 TARGET_TAG=$(eval echo "$ORB_EVAL_TARGET_TAG")
 
 IMAGE_ROOT="$ORB_VAL_REGISTRY_URL/${!ORB_ENV_PROJECT_ID}/$ORB_VAL_IMAGE"
+echo gcloud container images add-tag --quiet \
+    "$IMAGE_ROOT:$SOURCE_TAG" \
+    "$IMAGE_ROOT:$TARGET_TAG"
+
 gcloud container images add-tag --quiet \
     "$IMAGE_ROOT:$SOURCE_TAG" \
     "$IMAGE_ROOT:$TARGET_TAG"
